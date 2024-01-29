2b06962bfe72d74160cfa7e926efe5623c9dce69
Juliana <julianaraujo555@outlook.com>
2024-01-29 12:49:15 -0300

    resize menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,36 @@ const navMenu = document.querySelector(".nav--menu")
 console.log('OK')
 
 iconMenu.addEventListener('click', function(){
-	navMenu.classList.toggle('hidden');
+	if (navMenu.style.display === 'none' || navMenu.style.display === '') {
+		navMenu.style.display = 'block';
+		navMenu.style.height = 'auto';
+		navMenu.style.opacity = '1';
+	} else {
+		navMenu.style.display = 'none';
+		navMenu.style.height = '0';
+		navMenu.style.opacity = '0';
+	}
 });
+
+
+function toggleMenu() {
+	if (window.innerWidth > 790) {
+		if (navMenu.style.display === 'none' || navMenu.style.display === '') {
+			navMenu.style.display = 'flex';
+			navMenu.style.height = 'auto';
+			navMenu.style.opacity = '1';
+		} else {
+			navMenu.style.display = 'none';
+			navMenu.style.height = '0';
+			navMenu.style.opacity = '0';
+		}
+	} else {
+		navMenu.style.display = 'none';
+		navMenu.style.height = '0';
+		navMenu.style.opacity = '0';
+	}
+}
+
+toggleMenu();
+
+window.addEventListener('resize', toggleMenu);
